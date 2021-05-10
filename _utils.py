c6e9c6e8349d207392686f77298aec70d5dce4aa
Faiz Rahim <50950705+fwizzz@users.noreply.github.com>
2021-05-10 22:00:35 +0530

    Initial commit

---
+++ _utils.py
@@ -0,0 +1,18 @@
+
+api_url = "https://api.covid19india.org/csv/latest/states.csv"
+month_dict = {
+    "01": "Jan",
+    "02": "Feb",
+    "03": "March",
+    "04": "Apr",
+    "05": "Mar",
+    "06": "Jun",
+    "07": "Jul",
+    "08": "Aug",
+    "09": "Sept",
+    "10": "Oct",
+    "11": "Nov",
+    "12": "Dec"
+
+}
+
